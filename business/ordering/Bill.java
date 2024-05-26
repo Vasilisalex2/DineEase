355bc7038d9be1babc0adf2306730b4cc7c74017
teokon <74197733+teokon@users.noreply.github.com>
2024-05-26 14:06:22 +0300

    Add files via upload

--- business/ordering/Bill.java
+++ business/ordering/Bill.java
@@ -1,15 +1,42 @@
-package business.ordering;
-
-/**
- * brief description of the class.
- *
- * (if necessary)
- * detailed description of the class purpose, functionality, usage
- *
- * @author NAME
- * @version 1.0
- */
-
 public class Bill {
-  
+    private Order order;
+    private double totalPrice = 0;
+    private int tableId;
+    public Bill(Order order, int tableId) {
+        this.order = order;
+        totalPrice = getTotalPrice();
+     this.tableId = tableId;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public double getTotalPrice() {
+        return calculateTotalPrice();
+    }
+
+    private double calculateTotalPrice() {
+
+        for (Dish dish : order.getOrder()) {
+            totalPrice += dish.getPrice() * dish.getQuantity();
+        }
+        return totalPrice;
+    }
+
+    public String generateBillDetails() {
+        // Παράγετε τις λεπτομέρειες του τιμολογίου χρησιμοποιώντας τα δεδομένα του τιμολογίου
+        String billDetails = "Bill for Table ID: " + order.getTableId() + "\n" + "Order Items:\n";
+        for (Dish dish : order.getOrder()) {
+            double dishTotal = dish.getPrice() * dish.getQuantity();
+            billDetails += dish.getName() + " - Quantity: " + dish.getQuantity()
+                    + ", Price per unit: $" + dish.getPrice()
+                    + ", Total: $" + dishTotal + "\n";
+        }
+        billDetails += "Total Price: $" + getTotalPrice() + "\n";
+        return billDetails;
+    }
+    public int getTableId() {
+        return tableId;
+    }
 }
